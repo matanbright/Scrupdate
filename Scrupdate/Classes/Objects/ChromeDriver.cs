eb2079e4e0dc0aa89abb448dc50e5c176f65839e
Matan Brightbert <matana0000@gmail.com>
2024-10-29 08:51:04 +0200

    Fix resource leak on ChromeDriver quit

--- Scrupdate/Classes/Objects/ChromeDriver.cs
+++ Scrupdate/Classes/Objects/ChromeDriver.cs
@@ -122,7 +122,6 @@ namespace Scrupdate.Classes.Objects
                     chromeOptions.AddUserProfilePreference("profile.default_content_setting_values.images", 2);
                     chromeOptions.AddUserProfilePreference("profile.managed_default_content_settings.images", 2);
                 }
-                ChromeDriverService chromeDriverService = null;
                 try
                 {
                     chromeDriverService = ChromeDriverService.CreateDefaultService(
